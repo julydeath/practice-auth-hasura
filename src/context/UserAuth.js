3dcf9f716fff50e8283dd3b5c38470894d4924e7
julydeath <manojkarajada.mk@gmail.com>
2023-05-24 22:45:18 +0530

    init : userauth getting reasdy

--- src/context/UserAuth.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-export const UserAuthContext = createContext();
-
-export const UserAuthContextProvider = ({ children }) => {
-  return <UserAuthContext.Provider>{children}</UserAuthContext.Provider>;
-};
